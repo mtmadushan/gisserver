8d81d9330164ed5167a70037d9137848496a83b3
AUK Wijesinghe 🚀 <110575906+aukdev@users.noreply.github.com>
2024-04-09 17:46:30 +0530

    Update sequelize.js

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,6 +1,10 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('postgres://db_user:userpassword@localhost:5432/gis_application');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5435,
+});
 
 module.exports = sequelize;
